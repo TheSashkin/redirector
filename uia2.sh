32b5b88067dec2cb898966c03c216ad19d992d90
TheSashkin <74656533+TheSashkin@users.noreply.github.com>
2025-04-14 00:42:39 +0700

    Delete uia2.sh

--- uia2.sh
+++
@@ -1,9 +0,0 @@
-pkg upgrade -y
-pkg install git -y
-pkg install python -y
-pip install cython
-pkg install libxml2 libxslt -y
-pkg install -y python ndk-sysroot clang make libjpeg-turbo -y
-pkg install clang -y
-pip install lxml
-pip install --pre uiautomator2
